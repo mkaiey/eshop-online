848d78dddced3191e1eb46ee8b3dfdac4a113226
mkgit0000 <kaisarlord123@gmail.com>
2023-07-14 11:10:19 +0700

    backend url add

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,3 +1,3 @@
-export const server = "http://localhost:8000/api/v2";
+export const server = "https://eshop-online-id7i.vercel.app/api/v2";
 
-export const backend_url = "http://localhost:8000/";
+export const backend_url = "https://eshop-online-id7i.vercel.app/";
