afe394fb4ca265d74c0201431d7c5505dc85e4ff
mkgit0000 <kaisarlord123@gmail.com>
2023-07-14 12:11:23 +0700

    frontend patch update 2nd

--- backend/controller/shop.js
+++ backend/controller/shop.js
@@ -43,7 +43,7 @@ router.post("/create-shop", upload.single("file"), async (req, res, next) => {
 
     const activationToken = createActivationToken(seller);
 
-    const activationUrl = `http://localhost:3000/seller/activation/${activationToken}`;
+    const activationUrl = `https://eshop-online-77vr.vercel.app/seller/activation/${activationToken}`;
 
     try {
       await sendMail({
